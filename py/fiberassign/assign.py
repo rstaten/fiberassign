6d2cbe09e8f11ebef042d251e0bdef600da3020d
Theodore Kisner <tskisner.public@gmail.com>
2021-05-01 01:44:57 -0700

    Simplify plotting of fiberassign output.  The new fba_plot script just takes a list of filenames and gets the tile and run date information from the headers.

--- py/fiberassign/assign.py
+++ py/fiberassign/assign.py
@@ -687,7 +687,7 @@ def read_assignment_fits_tile(params):
     input target catalogs.
 
     Args:
-        params (tuple):  The tile ID and tile file path packed as a tuple for
+        params (tuple):  The tile file path packed as a tuple for
             use in multiprocessing.
 
     Returns:
@@ -695,7 +695,7 @@ def read_assignment_fits_tile(params):
             recarray, the available targets recarray, and the GFA targets
 
     """
-    (tile_id, tile_file) = params
+    (tile_file) = params
     log = Logger.get()
     # Output tile file
     if not os.path.isfile(tile_file):
@@ -1035,7 +1035,7 @@ def merge_results_tile(out_dtype, copy_fba, params):
     tm.start()
 
     inhead, fiber_data, targets_data, avail_data, gfa_targets = \
-        read_assignment_fits_tile((tile_id, infile))
+        read_assignment_fits_tile((infile))
 
     # tm.stop()
     # tm.report("  read input data {}".format(tile_id))
